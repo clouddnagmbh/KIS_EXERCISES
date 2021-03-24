33d82b1023641173ba86e5d4a872efdc3362b0dd
molzi <mo@clouddna.at>
2021-03-24 15:34:34 +0000

    Exercise 5.2 finished

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -2,23 +2,70 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/Fragment",
-	"sap/m/MessageBox"
-], function (Controller, JSONModel, Fragment, MessageBox) {
+	"sap/m/MessageBox",
+	"at/clouddna/training00/FioriDeepDive/formatter/formatter",
+	"sap/ui/core/routing/History"
+], function (Controller, JSONModel, Fragment, MessageBox, formatter, History) {
 	"use strict";
 
 	return Controller.extend("at.clouddna.training00.FioriDeepDive.controller.Customer", {
 
+		formatter: formatter,
+
 		_fragmentList: {},
+		_sMode: "",
 
 		onInit: function () {
 
+			let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+			oRouter.getRoute("Customer").attachPatternMatched(this._onPatternMatched, this);
+		},
+
+		_onPatternMatched: function (oEvent) {
 			let oEditModel = new JSONModel({
-				editmode: false
-			});
+					editmode: false
+				}),
+				sCustomerId = oEvent.getParameter("arguments").customerid;
 
 			this.getView().setModel(oEditModel, "editModel");
-			this._showCustomerFragment("DisplayCustomer");
 
+			if (sCustomerId !== "create") {
+				this._sMode = "display";
+				this._showCustomerFragment("DisplayCustomer");
+				this.getView().bindElement("/CustomerSet(guid'" + sCustomerId + "')");
+			} else {
+				this._sMode = "create";
+
+				let oCreateModel = new JSONModel({
+					Firstname: "",
+					Lastname: "",
+					AcademicTitle: "",
+					Gender: "M",
+					Email: "",
+					Phone: "",
+					Website: ""
+				});
+
+				this.getView().setModel(oCreateModel, "createModel");
+
+				oEditModel.setProperty("/editmode", true);
+				this._showCustomerFragment("CreateCustomer");
+			}
+
+		},
+
+		onNavBack: function (oEvent) {
+			let oHistory = History.getInstance(),
+				oPreviousHash = oHistory.getPreviousHash();
+
+			if (oPreviousHash) {
+				window.history.go(-1);
+			} else {
+				let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+				oRouter.navTo("Main", true);
+			}
 		},
 
 		onEditPress: function (oEvent) {
@@ -26,10 +73,53 @@ sap.ui.define([
 		},
 
 		onSavePress: function (oEvent) {
-			let oCustomer = this.getView().getModel().getProperty("/");
+			let oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 
-			this._toggleEdit(false);
-			MessageBox.information(JSON.stringify(oCustomer));
+			if (this._sMode === "create") {
+				let oModel = this.getView().getModel(),
+					oCreateData = this.getView().getModel("createModel").getData();
+
+				oModel.create("/CustomerSet", oCreateData, {
+					success: function (oData, response) {
+						MessageBox.information(oBundle.getText("dialog.create.success"), {
+							onClose: function () {
+								this.onNavBack();
+							}.bind(this),
+						});
+					}.bind(this),
+					error: function (oError) {
+						MessageBox.error(oError.message, {
+							onClose: function () {
+								this.onNavBack();
+							}.bind(this)
+						});
+					}.bind(this)
+				});
+			} else {
+				if (this.getView().getModel().hasPendingChanges()) {
+					this.getView().getModel().submitChange();
+					MessageBox.information(oBundle.getText("dialog.update.success"));
+				}
+			}
+		},
+
+		onCancelPress: function (oEvent) {
+			let oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+
+			MessageBox.confirm(oBundle.getText("dialog.cancel"), {
+				onClose: function (sAction) {
+					if (sAction === MessageBox.Action.OK) {
+						if (this._sMode === "create") {
+							this.onNavBack();
+						} else {
+							if (this.getView().getModel().hasPendingChanges()) {
+								this.getView().getModel().resetChanges();
+								this._toggleEdit(false);
+							}
+						}
+					}
+				}.bind(this)
+			});
 		},
 
 		_toggleEdit: function (bEditMode) {
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,44 +1,60 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("at.clouddna.training00.FioriDeepDive.controller.Main", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
 		onInit: function () {
+			let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+			oRouter.getRoute("Main").attachPatternMatched(this._onPatternMatched, this);
 
 		},
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+		_onPatternMatched: function (oEvent) {
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+		},
 
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf at.clouddna.training00.FioriDeepDive.view.Main
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		onNewCustomerPress: function (oEvent) {
+			let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+			oRouter.navTo("Customer", {
+				customerid: "create"
+			}, false);
+		},
+
+		onCustomerPress: function (oEvent) {
+			let oRouter = sap.ui.core.UIComponent.getRouterFor(this),
+				sCustomerId = oEvent.getSource().getBindingContextPath().split("'")[1];
+
+			oRouter.navTo("Customer", {
+				customerid: sCustomerId
+			}, false);
+		},
+
+		onDeleteCustomerPress: function (oEvent) {
+			let oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle(),
+				sCustomerPath = oEvent.getSource().getBindingContext().sPath,
+				oModel = this.getView().getModel();
+
+			MessageBox.confirm(oBundle.getText("dialog.delete"), {
+				onClose: function (sAction) {
+					if (sAction === MessageBox.Action.OK) {
+						oModel.remove(sCustomerPath, {
+							success: function (oData, reponse) {
+								MessageBox.success(oBundle.getText("dialog.delete.success"));
+							},
+							error: function (oError) {
+								console.log(oError);
+								MessageBox.error(oError.message);
+							}
+						});
+					}
+				}
+			});
+		}
 
 	});
 
